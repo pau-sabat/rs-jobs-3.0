6a7f912d554b615aeccd96078d06712cf4cf6b79
Pau Sàbat <pau.sabat@solvethex.com>
2025-08-20 12:50:49 +0200

    fix: swiper init

--- public/js/testimonials-swiper.js
+++ public/js/testimonials-swiper.js
@@ -1,17 +1,19 @@
-var swiper = new Swiper('.testimonials-swiper', {
-	slidesPerView: 1,
-	spaceBetween: 20,
-	loop: true,
-	autoplay: {
-		delay: 5000,
-		disableOnInteraction: false,
-	},
-	grabCursor: true,
-	watchOverflow: true,
-	observer: true,
-	observeParents: true,
-	pagination: {
-		el: '.swiper-pagination',
-		clickable: true,
-	},
+document.addEventListener('DOMContentLoaded', () => {
+	new Swiper('.testimonials-swiper', {
+		slidesPerView: 1,
+		spaceBetween: 20,
+		loop: true,
+		autoplay: {
+			delay: 5000,
+			disableOnInteraction: false,
+		},
+		grabCursor: true,
+		watchOverflow: true,
+		observer: true,
+		observeParents: true,
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+	})
 })
