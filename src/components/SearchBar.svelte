1d09eeb35fb98360aa3d3caa166cefafc9747a83
Pau Sàbat <pau.sabat@solvethex.com>
2025-08-25 14:32:55 +0200

    refactor select styles

--- src/components/SearchBar.svelte
+++ src/components/SearchBar.svelte
@@ -2,7 +2,7 @@
 	export let search = true
 	export let location = true
 	export let button = true
-	export let onSearch = (searchText) => {}
+	export let onSearch = searchText => {}
 
 	let searchText = ''
 
@@ -17,19 +17,19 @@
 			<div class="flex items-center justify-center w-6 h-6">
 				<img src="/assets/images/search-icon.svg" alt="Buscar" class="w-4 h-4" />
 			</div>
-            <input type="text" placeholder="Puesto de trabajo o palabra clave" bind:value={searchText} name="search" class="flex-grow text-small-mobile md:text-small-desktop py-1.5" />
+			<input type="text" placeholder="Puesto de trabajo o palabra clave" bind:value={searchText} name="search" class="flex-grow text-small-mobile md:text-small-desktop py-1.5" />
 		</div>
 	{/if}
 
 	{#if location}
 		<div class="flex items-center gap-2 w-full flex-grow">
 			<div class="flex items-center justify-center w-6 h-6">
-				<img src="/assets/images/location-icon.svg" alt="Ubicación" class="w-3.5 h-5">
+				<img src="/assets/images/location-icon.svg" alt="Ubicación" class="w-3.5 h-5" />
 			</div>
-            <select name="location-select" class="custom-select pr-[40px] flex-grow text-small-mobile md:text-small-desktop py-1.5">
-                <option value="">Selecciona la provincia</option>
-            </select>
-        </div>
+			<select name="location-select" class="flex-grow text-small-mobile md:text-small-desktop py-1.5 border-b border-secondary/50">
+				<option value="">Selecciona la provincia</option>
+			</select>
+		</div>
 	{/if}
 
 	{#if button}
