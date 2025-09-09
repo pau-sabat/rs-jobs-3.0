d24bf7e0a005328d3b02db4da01898d7bc0b9fd6
Pau Sàbat <pau.sabat@solvethex.com>
2025-09-09 13:43:15 +0200

    refactor: componentize toggle Switch

--- src/components/Alert.svelte
+++ src/components/Alert.svelte
@@ -1,4 +1,6 @@
 <script>
+	import Switch from './Switch.svelte'
+
 	export let active = false
 	export let title = 'Activar alerta'
 	export let text = ''
@@ -21,10 +23,7 @@
 			{/if}
 		</div>
 
-		<div class="relative inline-block w-9 h-5">
-			<input bind:checked={active} id="switch-component" type="checkbox" class="peer appearance-none w-9 h-5 bg-gray/15 rounded-full cursor-pointer transition-colors duration-300" />
-			<label for="switch-component" class="absolute top-1/2 -translate-y-1/2 left-1 w-4 h-4 bg-secondary/50 rounded-full border border-gray/15 shadow-sm transition-all duration-300 peer-checked:translate-x-3 peer-checked:bg-blue cursor-pointer"> </label>
-		</div>
+		<Switch bind:active />
 	</div>
 	{#if !compact}
 		<div class="flex flex-col">
---
+++ src/components/Switch.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let active = false
+</script>
+
+<div class="relative inline-block w-9 h-5">
+	<input
+        bind:checked={active}
+        id="switch-component"
+        type="checkbox"
+        class="peer appearance-none w-9 h-5 bg-gray/15 rounded-full cursor-pointer transition-colors duration-300"
+    />
+	<label for="switch-component" class="absolute top-1/2 -translate-y-1/2 left-1 w-4 h-4 bg-secondary/50 rounded-full border border-gray/15 shadow-sm transition-all duration-300 peer-checked:translate-x-3 peer-checked:bg-blue cursor-pointer"> </label>
+</div>
