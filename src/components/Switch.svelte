66e0550ab38ed762c26fd81ad7d00aeb547c25ca
Pau Sàbat <pau.sabat@solvethex.com>
2025-09-09 17:40:51 +0200

    fix: switch click behaviour

--- src/components/Switch.svelte
+++ src/components/Switch.svelte
@@ -9,5 +9,6 @@
         type="checkbox"
         class="peer appearance-none w-9 h-5 bg-gray/15 rounded-full cursor-pointer transition-colors duration-300"
     />
-	<label for="switch-component" class="absolute top-1/2 -translate-y-1/2 left-1 w-4 h-4 bg-secondary/50 rounded-full border border-gray/15 shadow-sm transition-all duration-300 peer-checked:translate-x-3 peer-checked:bg-blue cursor-pointer"> </label>
+	<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+	<label for="switch-component" on:click|preventDefault on:keydown|preventDefault class="absolute top-1/2 -translate-y-1/2 left-1 w-4 h-4 bg-secondary/50 rounded-full border border-gray/15 shadow-sm transition-all duration-300 peer-checked:translate-x-3 peer-checked:bg-blue cursor-pointer"> </label>
 </div>
