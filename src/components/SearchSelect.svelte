640e42b387945513ae286bae21c892037032c1d0
Pau Sàbat <pau.sabat@solvethex.com>
2025-09-10 17:47:10 +0200

    fix: searchSelect icon color

--- src/components/SearchSelect.svelte
+++ src/components/SearchSelect.svelte
@@ -33,7 +33,7 @@
 <div class="relative">
 	<div class="flex items-center gap-2 w-full flex-grow">
 		<div class="flex items-center justify-center w-6 h-6">
-			<Icon name="search" className="w-4 h-4 text-blue" />
+			<Icon name="search" className="w-4 h-4 text-secondary" />
 		</div>
 		<label for="search" class="sr-only">{placeholder}</label>
 		<input type="text" {placeholder} bind:value={searchText} name="search" class="flex-grow text-small-mobile md:text-small-desktop py-1.5" on:focus={handleInputFocus} on:blur={handleInputBlur} />
