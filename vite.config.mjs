bfa4624b9445844c7ce3416ba39ea4ebcf4dfd9f
Pau Sàbat <pau.sabat@solvethex.com>
2025-09-17 18:06:31 +0200

    fix: TS config

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { sveltePreprocess } from 'svelte-preprocess'
 
 export default defineConfig({
 	root: 'src',
 	plugins: [
 		svelte({
-			preprocess: {
-				typescript: true
-			}
+			preprocess: sveltePreprocess({
+				typescript: {
+					tsconfigFile: './tsconfig.json'
+				}
+			})
 		})
 	],
 	build: {
