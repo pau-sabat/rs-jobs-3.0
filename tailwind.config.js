c039f6f8bb159ea7d47c15e4ade7a24b12c9d7d5
Pau Sàbat <pau.sabat@solvethex.com>
2025-09-17 16:15:56 +0200

    prepare for production environment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./src/**/*.{js,svelte,ts}",
     "./src/views/**/*.pug",
     "./public/**/*.{html,js,css}",
     "./app.js"
