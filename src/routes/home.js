266201f1d1d077760850cbcf61ed53d376b08a92
Pau Sàbat <pau.sabat@solvethex.com>
2025-09-19 11:03:46 +0200

    refactor: home page without external json data

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const fs = require('fs')
-const path = require('path')
 
 // Ruta principal de la página de inicio
 router.get('/', (req, res) => {
-	const data = JSON.parse(fs.readFileSync(path.join(__dirname, '../../data/home.json')))
-	res.render('pages/home', { data })
+	res.render('pages/home')
 })
 
 module.exports = router
